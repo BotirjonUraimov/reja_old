546bca71d298230001db62e1d6d2f81905c2b866
BotirjonUraimov <botirjon.uraimov.93@gmail.com>
2023-01-03 00:44:59 +0900

    express

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express(); // expressning objectini ushlab olish uchun
 const http = require('http');
 // 1 Kirish
 app.use(express.static("public"));
+app.use(express.static(__dirname + "public"))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -15,12 +16,12 @@ app.set("view engine", "ejs");
 
 //routing code 
 app.get("/", function (req, res) {
-    res.end( `<h1 style="background:red;">Hello world by me</h1>` );
+    res.render( "project" );
 });
 
-app.get("/gift", function (req, res) {
-    res.end( `<h1 style="background:red;">bu sovg'alar sahifasi</h1>` );
-});
+//app.get("/gift", function (req, res) {
+//    res.end( `<h1 style="background:red;">bu sovg'alar sahifasi</h1>` );
+//});
 
 const server = http.createServer(app);
 let PORT = 3000;
