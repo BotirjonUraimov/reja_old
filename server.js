e7403c36a693e191c3474fd5cb615b7b9f0f10ce
BotirjonUraimov <botirjon.uraimov.93@gmail.com>
2023-01-04 22:46:32 +0900

    BRR feat: develop project router ejs page

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express(); // expressning objectini ushlab olish uchun
 const http = require('http');
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
 // 1 Kirish
 app.use(express.static("public"));
-app.use(express.static(__dirname + "public"))
+app.use(express.static(__dirname + "public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -15,8 +27,10 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //routing code 
-app.get("/", function (req, res) {
-    res.render( "project" );
+
+
+app.get("/portfolio", function (req, res) {
+    res.render( "project", {user: user} );
 });
 
 //app.get("/gift", function (req, res) {
@@ -28,3 +42,4 @@ let PORT = 3000;
 server.listen(PORT, function() {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
+
--- train.js
+++ train.js
@@ -1,2 +1,26 @@
+const { escapeSelector } = require("jquery");
+
 console.log("train task ishga tushdi");
 // D-task
+
+
+function calculator (a, b, c) {
+let errMsg = "not valid to operate!"
+ if( a == undefined || a == NaN ||c == undefined || c == NaN) {
+    return errMsg;
+} else if (b === "+"){
+        return a + c;
+} else if (b === "-"){
+        return a - c;
+} else if (b === "/"){
+    if(c == 0){
+        return errMsg;
+    } else{
+        return a / c;}
+} else if (b === "*"){
+        return a * c
+}
+}
+
+hisob = calculator(4, "-", 0);
+console.log(hisob);
